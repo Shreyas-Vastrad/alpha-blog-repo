d40aa4ff9dfba11b277163a012cfefe6a2163c4d
Shreyas Vastrad <shreyasvastrad72@gmail.com>
2020-08-20 16:07:53 +0530

    Interface_change

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,35 @@ class ArticlesController < ApplicationController
    def index
       @articles = Article.all 
    end
+   def new
+      @article = Article.new
+   end
+   def edit
+      @article = Article.find(params[:id])
+    end
+   def create
+      #render plain: params[:article]
+      @article = Article.new(params.require(:article).permit(:title, :description))
+      #render plain: @article.save
+      if @article.save
+         flash[:notice] = "Article was created successfully."
+         redirect_to article_path(@article) 
+       else
+         render 'new'
+       end
+   end
+   def update
+      @article = Article.find(params[:id])
+      if @article.update(params.require(:article).permit(:title, :description))
+        flash[:notice] = "Article was updated successfully."
+        redirect_to @article
+      else
+        render 'edit'
+      end
+    end
+    def destroy
+      @article = Article.find(params[:id])
+      @article.destroy
+      redirect_to articles_path
+   end
 end
